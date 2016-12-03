6b8d55afc35d9cf3268494c2bfdd828fd3a8946d
Unknown <tan.wongsiripiphat@gmail.com>
2016-12-03 15:59:15 +0700

    Add ApplicationShell

---
+++ DataVisualizer.UI/AppBootstrapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Caliburn.Micro;
+using DataVisualizer.UI.ApplicationShell;
+
+namespace DataVisualizer.UI
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        SimpleContainer container;
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            base.Configure();
+
+            container = new SimpleContainer();
+            container.Singleton<IWindowManager, WindowManager>();
+            container.Singleton<IEventAggregator, EventAggregator>();
+            container.Singleton<IApplicationShell, ApplicationShellViewModel>();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            base.OnStartup(sender, e);
+
+            DisplayRootViewFor<IApplicationShell>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            var instance = container.GetInstance(service, key);
+            if (instance != null)
+                return instance;
+            throw new InvalidOperationException("Could not locate any instances.");
+        }
+    }
+}
---
+++ DataVisualizer.UI/ApplicationShell/ApplicationShellViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace DataVisualizer.UI.ApplicationShell
+{
+    public class ApplicationShellViewModel : PropertyChangedBase, 
+        IApplicationShell
+    {
+    }
+}
